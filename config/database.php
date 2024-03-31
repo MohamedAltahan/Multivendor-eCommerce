783c99f5244271e117d5c8c31cf857cbda14bd4e
mohamed <medo85559@gmail.com>
2024-03-31 23:03:28 +0200

    m

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Database Connections
+    | Database Connections mm
     |--------------------------------------------------------------------------
     |
     | Here are each of the database connections setup for your application.
@@ -125,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
