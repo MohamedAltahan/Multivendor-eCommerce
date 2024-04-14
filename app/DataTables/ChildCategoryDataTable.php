a6ef0157cd909c21321d0cdf22978f42af4040a4
mohamed <medo85559@gmail.com>
2024-04-14 13:55:20 +0200

    ChildCategoryDataTable

--- app/DataTables/ChildCategoryDataTable.php
+++ app/DataTables/ChildCategoryDataTable.php
@@ -57,7 +57,7 @@ class ChildCategoryDataTable extends DataTable
      */
     public function query(ChildCategory $model): QueryBuilder
     {
-        return $model->newQuery();
+        return $model->orderBy('sub_category_id')->newQuery();
     }
 
     /**
